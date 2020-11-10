cc14cbbb70862ac0e5da631fcbb1e1c36eaca043
Paulo Albuquerque <paulo.albuquerque@pixter.com.br>
2020-11-10 10:11:25 -0300

    Criando metodos

--- lambda/index.js
+++ lambda/index.js
@@ -4,23 +4,95 @@
 const Alexa = require('ask-sdk-core');
 const axios = require('axios');
 
-const GetRemoteDataHandler = {
-  canHandle(handlerInput) {
-      console.log("GetRemoteDataHandler", handlerInput)
-    return handlerInput.requestEnvelope.request.type === 'LaunchRequest'
-      || (handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'GetRemoteDataIntent');
-  },
-  async handle(handlerInput) {
-    let outputSpeech = 'This is the default message.';
+const auth = () => {
+  
+  const url = "https://idcs-902a944ff6854c5fbe94750e48d66be5.identity.oraclecloud.com/oauth2/v1/token";
+  const headers = {
+    "authorization": "Basic OThiMmEwZDY0MWQ0NDFmZDhmMWQyOTdlNDg3NjFmMzk6ZDczMjU5YWYtYzJhZC00MTMzLWI0NjEtNDYyN2IwN2VlMDZj",
+    "cache-control": "no-cache",
+    "content-type": "application/x-www-form-urlencoded",
+    "postman-token": "280d6ac2-0e1c-d7ed-fc20-85de145f3d1c"
+  }
 
-    await axios.post('http://ec2-54-159-213-8.compute-1.amazonaws.com:1880/alexa', handlerInput)
+  return await axios({
+    method: 'post',
+    url,
+    params: handlerInput,
+    headers
+  })
+  .then(function (response) {
+    return {
+      access_token: response.data.access_token,
+      token_type: response.data.token_type
+    }
+  })
+  .catch(function (error) {
+    console.log(`ERROR: ${error.message}`);
+  });
+
+}
+
+const InitialIntent = (data) => {
+
+  return [
+    'O que você gostaria de fazer?',
+    '- Acessar conta',
+    '- Noticias da manha',
+    '- Solicitar abertura de conta'
+  ]
+  await axios.post('http://ec2-54-159-213-8.compute-1.amazonaws.com:1880/alexa', handlerInput)
     .then(function (response) {
       outputSpeech = response.data.payload
     })
     .catch(function (error) {
       console.log(`ERROR: ${error.message}`);
     });
+}
+
+const AccountIntent = (data) => {
+  await axios.post('http://ec2-54-159-213-8.compute-1.amazonaws.com:1880/alexa', handlerInput)
+    .then(function (response) {
+      outputSpeech = response.data.payload
+    })
+    .catch(function (error) {
+      console.log(`ERROR: ${error.message}`);
+    });
+}
+
+const NewsIntent = (data) => {
+  await axios.post('http://ec2-54-159-213-8.compute-1.amazonaws.com:1880/alexa', handlerInput)
+    .then(function (response) {
+      outputSpeech = response.data.payload
+    })
+    .catch(function (error) {
+      console.log(`ERROR: ${error.message}`);
+    });
+}
+
+const NewAccountIntent = (data) => {
+  await axios.post('http://ec2-54-159-213-8.compute-1.amazonaws.com:1880/alexa', handlerInput)
+    .then(function (response) {
+      outputSpeech = response.data.payload
+    })
+    .catch(function (error) {
+      console.log(`ERROR: ${error.message}`);
+    });
+}
+
+const GetHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'LaunchRequest'
+      || (handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && (
+          handlerInput.requestEnvelope.request.intent.name === 'AccountIntent' ||
+          handlerInput.requestEnvelope.request.intent.name === 'NewsIntent' ||
+          handlerInput.requestEnvelope.request.intent.name === 'NewAccountIntent'
+        )
+      );
+  },
+  async handle(handlerInput) {
+
+    const outputSpeech = this[handlerInput.requestEnvelope.request.intent.name](handlerInput);
 
     return handlerInput.responseBuilder
       .speak(outputSpeech)
@@ -31,7 +103,6 @@ const GetRemoteDataHandler = {
 
 const HelpIntentHandler = {
   canHandle(handlerInput) {
-      console.log("HelpIntentHandler", handlerInput)
     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
       && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
   },
@@ -47,7 +118,6 @@ const HelpIntentHandler = {
 
 const CancelAndStopIntentHandler = {
   canHandle(handlerInput) {
-      console.log("CancelAndStopIntentHandler", handlerInput)
     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
       && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
         || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
@@ -63,7 +133,6 @@ const CancelAndStopIntentHandler = {
 
 const SessionEndedRequestHandler = {
   canHandle(handlerInput) {
-      console.log("SessionEndedRequestHandler", handlerInput)
     return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
   },
   handle(handlerInput) {
@@ -91,7 +160,7 @@ const skillBuilder = Alexa.SkillBuilders.custom();
 
 exports.handler = skillBuilder
   .addRequestHandlers(
-    GetRemoteDataHandler,
+    GetHandler,
     HelpIntentHandler,
     CancelAndStopIntentHandler,
     SessionEndedRequestHandler,
